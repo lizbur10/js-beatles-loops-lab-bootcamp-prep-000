01bf3a6ca40db4625faff443b92aa60f0b15aed1
Liz Burton <liz.burton147@gmail.com>
2019-08-22 17:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function theBeatlesPlay(musicians, instruments) {
   //This is NOT what we want to do!!
   //const array = ['John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums'];
   const array = [];
-  
+  for () {
+    
+  }
   return array;
 }
